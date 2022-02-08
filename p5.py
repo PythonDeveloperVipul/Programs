327249f2c951ecfae6b91d596b0697c85e0bc683
PythonDeveloperVipul <98796100+PythonDeveloperVipul@users.noreply.github.com>
2022-02-08 00:32:37 -0800

    Update p5.py

--- p5.py
+++ p5.py
@@ -7,7 +7,5 @@ list1=[56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
 #         res.append(item)
 # print(res)
 
-# it is practise code
-a=10
 ans=[item for item in list1 if math.isnan(item)==False]
 print(ans)
